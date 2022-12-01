1cbd2aa322711b3958999da534a15f2216f83841
nvlaminc <noemie.vlaminck@cenaero.be>
2022-12-01 17:30:39 +0100

    generalise the vector dimensions to work in several situations

--- DimensionReduction/utils.py
+++ DimensionReduction/utils.py
@@ -40,10 +40,10 @@ def gen_array(path):
     x = [df[i*n_t: (i+1)*n_t] for i in range(n_s)]
     return np.asarray(x)
 
-def gen_dwt(sig, width= np.arange(1,25)):
+def gen_dwt(sig, width= np.arange(1,25), time_basis = 24):
     dwt = []
     # mean per participant for each week
-    s = sig[:, :7*24*76].reshape(len(sig), -1, 7, 24).mean(1)
+    s = sig[:, :7*time_basis*(sig.shape[-1]//(time_basis*7))].reshape(len(sig), -1, 7, time_basis).mean(1)
     for i in tqdm(range(len(sig))):
         # generate the discrete wavelet tranform + downsampling
         cwt = signal.cwt(s[i].flatten(), signal.ricker, width)[:, ::2]
@@ -64,48 +64,44 @@ def gen_apca(sig, window):
         apca.append(piecewise_approx(sig[i], window))
     return np.asarray(apca)
 
-def plot_cluster(dataset, cluster, cluster_id=0, ploperiod='day'):
+def plot_cluster(dataset, cluster, cluster_id=0, period='day', time_basis = 24):
     fig = plt.figure()
     population = (cluster == cluster_id).sum()
-    
     if period == 'day':
         # Reshape per days
-        x = dataset[cluster==cluster_id].reshape(population, -1, 24)
+        x = dataset[cluster==cluster_id].reshape(population, -1, time_basis)
         for i in range(population):
             # plot the mean consumption per day for each element
             #of the population of the cluster.
             plt.plot(x[i].mean(0), c='black', linewidth=0.5, alpha=0.5)
         plt.plot(x.mean(1).mean(0))
-        plt.suptitle(f'Daily Energy Consumption Profile of the {cluster_id+1} Cluster')
-    
+        plt.suptitle(f'Daily Energy Consumption Profile of the Cluster {cluster_id+1}')
     elif period == 'week':
         # Reshape per weeks remove last days to have a multiple
         #of a week (i.e. 7 days).
-        x = dataset[cluster==cluster_id][:, :7*24*76].reshape(population, -1, 7, 24)
+        x = dataset[cluster==cluster_id][:, :7*time_basis*(dataset.shape[-1]//(time_basis*7))].reshape(population, -1, 7, time_basis)
         for i in range(population):
             plt.plot(x[i].mean(0).flatten(), c='black', linewidth=0.25, alpha=0.75)
         plt.plot(x.mean(1).mean(0).flatten())
-        plt.suptitle(f'Weekly Energy Consumption Profile of the {cluster_id+1} Cluster')
-        
+        plt.suptitle(f'Weekly Energy Consumption Profile of the Cluster {cluster_id+1}')    
     ax = plt.gca()
     ax.set_ylim([0, 4])
-        
     plt.grid()
     plt.xlabel('Time')
     plt.ylabel('Electrical Consumption')
     plt.show()
     
-def plot_clusters(dataset, cluster):
+def plot_clusters(dataset, cluster, time_basis = 24):
     n_cluster = len(np.unique(cluster))
     fig, ax = plt.subplots(n_cluster, 2, figsize=(10, 20))
     for j in range(n_cluster):
-        sig = dataset[cluster==j].reshape((cluster==j).sum(), -1, 24)
+        sig = dataset[cluster==j].reshape((cluster==j).sum(), -1, time_basis)
         for i in range((cluster==j).sum()):
             ax[j][0].plot(sig[i].mean(0), c='black', linewidth=0.5, alpha=0.5)
         ax[j][0].grid()
         ax[j][0].plot(sig.mean(0).mean(0))
         ax[j][0].set_ylim([0, 5])
-        sig = dataset[cluster==j][:, :7*24*(dataset.shape[-1]//(24*7))].reshape((cluster==j).sum(), -1, 7, 24)
+        sig = dataset[cluster==j][:, :7*time_basis*(dataset.shape[-1]//(time_basis*7))].reshape((cluster==j).sum(), -1, 7, time_basis)
         for i in range((cluster==j).sum()):
             ax[j][1].plot(sig[i].mean(0).flatten(), c='black', linewidth=0.5, alpha=0.5)
         ax[j][1].grid()
@@ -115,8 +111,7 @@ def plot_clusters(dataset, cluster):
     ax[j][1].set_xlabel('Time [h]')
     plt.suptitle(f'Visualisation of the {n_cluster} clusters profile', y=0.915)
     plt.show()
-    
-    
+       
 def rmse_var(x, x_hat):
     return np.sqrt( np.mean((x - x_hat)**2)) / np.mean(x)
 
@@ -141,11 +136,9 @@ def seasons_reshape(x):
 
     return season.mean(2).reshape(season.shape[0], -1)
 
-
 def gaussian_smoothing (df, std = 3, n_points = 25):
     df_smoothed = df.copy()
     df_smoothed = df_smoothed.rolling(n_points, min_periods = 1, center=True, win_type = "gaussian").mean(std = std)
-
     return df_smoothed
 
 def compute_intersect(series_a, series_b, intersect=True):
